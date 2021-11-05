bugfix to use the correct number of rotation steps

lifesim/instrument/instrument.py
@@ -648,13 +648,13 @@ class Instrument(InstrumentModule):
         flux_planet = (flux_planet_spectrum[:, np.newaxis]
                        * transm_curve_chop
                        * integration_time
-                       / len(transm_curve_chop)
+                       / self.data.options.other['rotation_steps']
                        * self.data.inst['eff_tot']
                        * self.data.inst['telescope_area']
                        * self.data.inst['wl_bin_widths'][:, np.newaxis])
         noise_planet = (flux_planet_spectrum[:, np.newaxis]
                         * transm_curve_tm4
-                        / len(transm_curve_tm4)
+                        / self.data.options.other['rotation_steps']
                         * integration_time
                         * self.data.inst['eff_tot']
                         * self.data.inst['telescope_area']
@@ -676,9 +676,9 @@ class Instrument(InstrumentModule):
         else:
             noise_bg = noise_bg_list
 
-        noise_bg = noise_bg * integration_time * self.data.inst['eff_tot']
+        noise_bg = noise_bg * integration_time / self.data.options.other['rotation_steps'] * self.data.inst['eff_tot']
         noise_bg = np.repeat(noise_bg[:, np.newaxis], repeats=self.data.options.other['rotation_steps'], axis=1)
-        noise_rotation = noise_rotation * integration_time / len(transm_curve_chop)
+        noise_rotation = noise_rotation * integration_time / self.data.options.other['rotation_steps'] * self.data.inst['eff_tot']
 
         noise = (noise_bg + noise_rotation + noise_planet) * 2
 

lifesim_demo_inclined_ez.py
@@ -24,7 +24,7 @@ bus = lifesim.Bus()
 bus.data.options.set_scenario('baseline')
 
 # set options manually
-#bus.data.options.set_manual(rotation_steps=15)
+bus.data.options.set_manual(rotation_steps=360)
 
 # ---------- Creating the Instrument ----------
 
@@ -116,8 +116,8 @@ flux_planet_spectrum = [wl_bins * u.meter, fgamma]
                                            flux_planet_spectrum=flux_planet_spectrum,
                                            integration_time=integration_time,
                                            safe_mode=False,
-                                           inclination_ez=45./180.*np.pi,
-                                           ascending_node_ez=45./180.*np.pi)
+                                           inclination_ez=0./180.*np.pi,
+                                           ascending_node_ez=0./180.*np.pi)
 
 snri = np.sqrt(np.sum(spectrum[1] ** 2))
 
@@ -126,15 +126,15 @@ bus.connect(('inst', 'exo'))
 
 (spectrum,
  flux_planet,
- noise) = instrument.get_spectrum_inclined(temp_s=temp_s,  # for description, view get_spectrum documentation
-                                           radius_s=radius_s,
-                                           distance_s=distance_s,
-                                           lat_s=lat_s,
-                                           z=z,
-                                           angsep=angsep,
-                                           flux_planet_spectrum=flux_planet_spectrum,
-                                           integration_time=integration_time,
-                                           safe_mode=False)
+ noise) = instrument.get_spectrum(temp_s=temp_s,  # for description, view get_spectrum documentation
+                                  radius_s=radius_s,
+                                  distance_s=distance_s,
+                                  lat_s=lat_s,
+                                  z=z,
+                                  angsep=angsep,
+                                  flux_planet_spectrum=flux_planet_spectrum,
+                                  integration_time=integration_time,
+                                  safe_mode=False)
 
 snr = np.sqrt(np.sum(spectrum[1] ** 2))
 