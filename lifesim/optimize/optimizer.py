222ff755bf1bbe921a6f59a5443af8c00d38e598
Felix A. Dannert <62441325+fdannert@users.noreply.github.com>
2025-02-24 16:03:37 +0100

    Update calculation method for `num_universe` in optimizer logic
    Replaced `max() + 1` with `len(np.unique())` to ensure accurate universe count. This change improves reliability and aligns with the underlying data structure's expectations.

--- lifesim/optimize/optimizer.py
+++ lifesim/optimize/optimizer.py
@@ -69,7 +69,7 @@ class Optimizer(OptimizationModule):
         # sum of detected planets per stype
         self.data.optm['sum_detected'] = np.zeros(5)
 
-        self.data.optm['num_universe'] = self.data.catalog.nuniverse.max() + 1
+        self.data.optm['num_universe'] = len(np.unique(self.data.catalog.nuniverse))
         self.data.optm['hit_limit'] = ((self.data.optm['sum_detected']
                                        / (self.data.optm['num_universe']))
                                        >= np.array(list(self.data.options.optimization['limit'].values())))
