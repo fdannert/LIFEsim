e98329c60b81b5afb15a13cbf26ea34455f9355d
Felix A. Dannert <62441325+fdannert@users.noreply.github.com>
2025-05-16 14:07:24 +0200

    added unsafe load fallback

--- lifesim/core/core.py
+++ lifesim/core/core.py
@@ -475,8 +475,10 @@ class Bus(object):
     def build_from_config(self,
                           filename: str):
         with open(filename) as file:
-
-            config_dict = yaml.load(file, Loader=yaml.FullLoader)
+            try:
+                config_dict = yaml.load(file, Loader=yaml.FullLoader)
+            except:
+                config_dict = yaml.unsafe_load(file)
         #     except:
         #         config_dict = yaml.unsafe_load(file)
         #
