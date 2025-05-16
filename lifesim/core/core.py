887954b555c78b76f460ccbd51c50214c6f4c28a
Felix A. Dannert <62441325+fdannert@users.noreply.github.com>
2025-05-16 14:05:22 +0200

    removed uneccessary config loader checks

--- lifesim/core/core.py
+++ lifesim/core/core.py
@@ -475,15 +475,15 @@ class Bus(object):
     def build_from_config(self,
                           filename: str):
         with open(filename) as file:
-            try:
-                config_dict = yaml.load(file, Loader=yaml.FullLoader)
-            except:
-                config_dict = yaml.unsafe_load(file)
 
-        if ((config_dict is None)
-                or (not isin(['array', 'optimization', 'models', 'other'], config_dict.keys()).all())):
-            print(config_dict)
-            raise ValueError(f'Config file {filename} could not be read.')
+            config_dict = yaml.load(file, Loader=yaml.FullLoader)
+        #     except:
+        #         config_dict = yaml.unsafe_load(file)
+        #
+        # if ((config_dict is None)
+        #         or (not isin(['array', 'optimization', 'models', 'other'], config_dict.keys()).all())):
+        #     print(config_dict)
+        #     raise ValueError(f'Config file {filename} could not be read.')
 
         self.data.options.array = convert_to_np(config_dict['array'])
         self.data.options.optimization = convert_to_np(config_dict['optimization'])
