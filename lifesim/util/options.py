56d284160053b1ad7ff7ac465c9e75864d96d3ba
Felix A. Dannert <62441325+fdannert@users.noreply.github.com>
2025-11-04 15:52:31 +0100

    first running implementation of multiprocessing, validated against single processing runs on small catalogs

--- lifesim/util/options.py
+++ lifesim/util/options.py
@@ -76,7 +76,8 @@ class Options(object):
                       'n_plugins': 0,
                       'output_path': None,
                       'output_filename': None,
-                      'fov_threshold': 0.}
+                      'fov_threshold': 0.,
+                      'n_cpu': 1}
 
         self.models = {'localzodi': '',
                        'habitable': '',
@@ -120,8 +121,7 @@ class Options(object):
 
         self.optimization['N_pf'] = 25
         self.optimization['snr_target'] = 7
-        self.optimization['limit'] = {'A': np.inf,
-                                      'F': np.inf,
+        self.optimization['limit'] = {'F': np.inf,
                                       'G': np.inf,
                                       'K': np.inf,
                                       'M': np.inf}
