958e5dd35fd9eed6342adfab09699a49d468cd21
kseliner <kseliner@ethz.ch>
2026-04-14 11:43:36 +0200

    thermal noise fixes

--- lifesim/instrument/pn_thermal.py
+++ lifesim/instrument/pn_thermal.py
@@ -1,6 +1,5 @@
 import numpy as np
 from typing import Union
-from lifesim.util import constants
 
 from lifesim.core.modules import PhotonNoiseInstrumentModule
 from lifesim.util.radiation import black_body
@@ -8,7 +7,7 @@ from lifesim.util.radiation import black_body
 
 class PhotonNoiseThermal(PhotonNoiseInstrumentModule):
     """
-    This class simulates the thermal noise contribution of the mirror to the interferometric
+    This class simulates the thermal noise contribution of the mirror and detector to the interferometric
     measurement of LIFE.
     """
 
@@ -45,26 +44,32 @@ class PhotonNoiseThermal(PhotonNoiseInstrumentModule):
         Notes
         -----
         All of the following parameters are needed for the calculation of the thermal mirror and detector noise
-        contribution and should be specified either in `data.catalog` or `data.single` or 'data.inst'.
+        contribution and should be specified either in 'data.inst' or 'data.options'.
 
+        data.inst['hfov'] : np.ndarray
+            Contains the half field of view of the observatory in [rad] for each of the spectral bins.
         data.inst['wl_bins'] : np.ndarray
             Central values of the spectral bins in the wavelength regime in [m].
         data.inst['wl_widths'] : np.ndarray
             Widths of the spectral wavelength bins in [m].
+        data.options.array['primary_temp'] : float
+            Temperature of the mirror in [K].
+        data.options.array['primary_emissivity'] : float
+            Emissivity of the mirror (dimensionless).
         data.inst['telescope_area'] : float
             Area of all array apertures combined in [m^2].
-        data.options.array['diameter'] : float
-            Diameter of the array in [m].
-        data.options.array['m_temp'] : float
-            Temperature of the mirror in [K].
-        data.options.array['m_emissivity'] : float
-            Emissivity of the mirror (dimensionless).
-        data.options.array['d_temp'] : float
-            Temperature of the detector in [K].
+        data.options.array['num_apertures'] : int
+            Number of apertures in the array.
         data.options.array['pixel_size'] : float
             Size of the pixels in [m]. (length of one side of the square pixel)
-        data.options.array['beam_size'] : float
-            Diameter of the beam in [m].
+        data.options.array['pix_per_wl'] : int
+            Number of pixels per wavelength bin (Nyquist rate).
+        data.options.array['detector_wl_min'] : float
+            Minimum wavelength of the detector sensitivity range in [m].
+        data.options.array['detector_wl_max'] : float
+            Maximum wavelength of the detector sensitivity range in [m].
+        data.options.array['d_temp'] : float
+            Temperature of the detector environment in [K].
         """
 
         # solid angle is governed by the fiber pick-up, which for single mode is lambda / D
@@ -78,7 +83,7 @@ class PhotonNoiseThermal(PhotonNoiseInstrumentModule):
 
         tm_leak = (solid_angle
                    * self.data.options.array['primary_emissivity']
-                   * self.data.inst['telescope_area']
+                   * self.data.inst['telescope_area'] / self.data.options.array['num_apertures']
                    * mirror_bb)
 
         # detector collects thermal noise photons across its whole sensitivity range (at least from the detector
@@ -87,7 +92,7 @@ class PhotonNoiseThermal(PhotonNoiseInstrumentModule):
 
         delta_wl = 1e-7
         total_area = self.data.options.array['pixel_size'] ** 2 * self.data.options.array['pix_per_wl'] # minimum number of detector pixels (nyquist rate)
-        solid_angle = 2 * np.pi  # half sphere, since the detector can receive photons from all directions
+        solid_angle = np.pi  # half sphere, considering angle relevant to the normal of the detector surface for the irradiance and spherical coordinates
 
         wl_bins = np.arange(self.data.options.array['detector_wl_min'],
                             self.data.options.array['detector_wl_max'],
